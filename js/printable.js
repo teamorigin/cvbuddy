13dd7a26e4180447a9499e5973c58622c8254bb9
hirushka <yasirukavishka96@gmail.com>
2018-09-17 04:00:11 +0530

    print-method added

---
+++ js/printable.js
@@ -0,0 +1,13 @@
+function printPages(elename) {
+    var eleContent = document.getElementById(elename).innerHTML;
+    w = window.open();
+    headContent = "<!DOCTYPE html><html><head><link href='css/materialize.css' media='screen,print' rel='stylesheet' type='text/css' /><link href='css/global.css' media='screen,print' rel='stylesheet' type='text/css'/><link href='css/global.css' rel='stylesheet' type='text/css' /></head><body>"
+    w.document.write(headContent);
+    w.document.write(eleContent);
+    endContent = "</body></html>";
+    w.document.write(endContent);
+    w.document.write('<scr' + 'ipt type="text/javascript">' + 'window.onload = function() { window.print(); window.close(); };' + '</sc' + 'ript>');
+    w.document.close();
+    w.focus();
+    return true;
+}
